a1d8060c58865b393937409ff8b0eb0054137aef
prasanna0054 <prasanna0054@outlook.com>
2023-08-08 17:09:41 +0100

    Docker Commit

--- src/test/java/com/sky/SpringDemo/SpringDemoApplicationTests.java
+++ src/test/java/com/sky/SpringDemo/SpringDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.sky.SpringDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringDemoApplicationTests {
 
 	@Test
